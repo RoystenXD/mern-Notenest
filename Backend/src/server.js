2f41d30d789dba510abda626126f5bf9d815de4d
Roysten T <santhanaroysten20@gmail.com>
2025-06-12 19:49:58 +0530

    prepared for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,26 +4,36 @@ import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import rateLimiter from './middleware/rateLimiter.js';
 import cors from 'cors';
+import path from 'path';
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5001;
 
+const __dirname = path.resolve(); // Get the current directory name
 
 //console.log(process.env.MONGO_URI);
-app.use(cors({
-    origin:"http://localhost:5173", // Allow requests from this origin
+if(process.env.NODE_ENV !== 'production') {
+    app.use(cors({
+        origin:"http://localhost:5173", // Allow requests from this origin
 }));
+}
 
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(rateLimiter); // Use the rate limiter middleware
 
 app.use('/api/notes',notesRoutes); 
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend","dist", "index.html"));
+});
+}// Serve static files from the public directory
 connectDB().then(() => {
     app.listen(PORT, () =>{
         console.log(`server is running on port on ${PORT}`);
 });
 });
 
-
-// mongodb+srv://santhanaroysten20:V2XfBPfOkWSuvyjo@cluster0.hxutjhs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
