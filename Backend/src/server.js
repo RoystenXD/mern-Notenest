21d534f5d504eac786c31b23b935648885777dc7
RoystenXD <santhanaroysten20@gmail.com>
2025-11-10 01:06:06 +0530

    Fixed folder name case for Render deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -25,10 +25,10 @@ app.use(rateLimiter); // Use the rate limiter middleware
 app.use('/api/notes',notesRoutes); 
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend","dist", "index.html"));
+        res.sendFile(path.join(__dirname, "../Frontend","dist", "index.html"));
 });
 }// Serve static files from the public directory
 connectDB().then(() => {
